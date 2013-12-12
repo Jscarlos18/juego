13befebdf015bb77cf186277590e5e25914d40ee
Jscarlos18 <jcquiancalle@hotmail.com>
2013-12-12 07:18:30 -0500

    colores

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,48 @@
 /*global define */
 define([], function () {
     'use strict';
+    var myArray = new Array();
+    var randomArray = new Array();
+    var colors = ["red", "blue", "yellow","green"];
+    
+    	
+        var highlight = function(button, color){
+        	var oldColor = button.css("background-color")
+        		button.css("background-color", color)
+        			.delay(200)
+        			.queue( function() {
+        				button.css("background-color", oldColor).dequeue();
+        			})
+        			}
+        var randomize = Math.floor(Math.random());
 
-    return '\'Allo \'Allo!';
+  $(document).ready(function(){
+    	$(".boton").click(function(){
+    		var thisId = $(this).attr('id')
+    		console.log(thisId)
+    		if(thisId ==='arriba-derecha') {
+    			myArray.push(thisId);
+    			highlight($(this) , "darkred")
+    			randomize;
+    			var colorsRandom = colors[randomize];
+    			randomArray.push(colorsRandom);
+    			
+    		} else if (thisId === "arriba-izquierda"){
+    			myArray.push(thisId);
+    			highlight($(this) , "darkgreen");
+    			
+    		}else if (thisId === "abajo-izquierda"){
+    			myArray.push(thisId);
+    			highlight($(this) , "#A6A600");
+    			
+    		}else if (thisId === "abajo-derecha"){
+    			myArray.push(thisId);
+    			highlight($(this) , "darkblue");
+    			
+    		} else {
+    			console.log("Wrong bton ID detected")
+    		}
+    		console.log(myArray);
+    	});
+    });
 });
