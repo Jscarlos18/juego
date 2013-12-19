c0d13cc001ba341bff1bf102d5e363f88d1f4ca3
Jscarlos18 <jcquiancalle@hotmail.com>
2013-12-19 06:24:46 -0500

    finish

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,48 +1,86 @@
-/*global define */
 define([], function () {
-    'use strict';
-    var myArray = new Array();
-    var randomArray = new Array();
-    var colors = ["red", "blue", "yellow","green"];
+    'use strict'
     
-    	
-        var highlight = function(button, color){
-        	var oldColor = button.css("background-color")
-        		button.css("background-color", color)
-        			.delay(200)
-        			.queue( function() {
-        				button.css("background-color", oldColor).dequeue();
-        			})
-        			}
-        var randomize = Math.floor(Math.random());
+    var userPlaying = false
+    var userClicks = new Array()
+    var buttonList
+    var computerSequence = new Array()
 
-  $(document).ready(function(){
-    	$(".boton").click(function(){
-    		var thisId = $(this).attr('id')
-    		console.log(thisId)
-    		if(thisId ==='arriba-derecha') {
-    			myArray.push(thisId)
-    			highlight($(this) , "darkred")
-    			randomize;
-    			var colorsRandom = colors[randomize];
-    			randomArray.push(colorsRandom);
-    			
-    		} else if (thisId === "arriba-izquierda"){
-    			myArray.push(thisId);
-    			highlight($(this) , "darkgreen");
-    			
-    		}else if (thisId === "abajo-izquierda"){
-    			myArray.push(thisId);
-    			highlight($(this) , "#A6A600");
-    			
-    		}else if (thisId === "abajo-derecha"){
-    			myArray.push(thisId);
-    			highlight($(this) , "darkblue");
-    			
-    		} else {
-    			console.log("Wrong bton ID detected")
-    		}
-    		console.log(myArray);
-    	});
-    });
+    var initialize = function() {
+        buttonList = jQuery.map( $(".boton"),
+                       function(element) {
+                         return $(element).attr('id')
+                       })
+    }
+
+    var generateComputerSequence = function() {
+        computerSequence.push( buttonList[
+                                 Math.floor(Math.random()
+                                   * buttonList.length)] )
+        console.log(computerSequence)
+    }
+
+    var highlight = function(button, color) {
+      var oldColor = button.css("background-color")
+      button.css("background-color", color).dequeue()
+            .delay(300)
+            .queue( function() {
+                    button.css("background-color", oldColor).dequeue()
+                  })
+    }
+
+    var showComputerSequence = function() {
+        var seq = computerSequence
+        for(var id in seq) {
+           (function(id){
+             setTimeout( function() {
+               highlight($("#"+seq[id]), "#AA66FF")//genera siguiente secuencia añadiendo uno mas a la secuencia anterior
+             }, 600*id)
+           })(id)
+        }
+        setTimeout( function() {
+            userPlaying = true
+        }, 600*seq.length)
+    }
+
+    var compareSequences = function() {
+        // TODO
+        return true
+    }
+
+    var endGame = function() {
+        // TODO
+    }
+    
+    $(document).ready(function() {
+        initialize()
+
+        generateComputerSequence()
+        showComputerSequence()
+
+        $('.boton').click( function() {
+            if(userPlaying) {
+                var thisId = $(this).attr('id')
+                highlight($(this), "#fff")
+                userClicks.push(thisId)
+
+                if (userClicks.length >= computerSequence.length) {//nº de clicks del jugador comparado con el nº de clicks de la computadora
+                    userPlaying = false 
+                    if (!compareSequences()){//si no son los correctos o se pasa de clicks se acaba el juego
+                        // FIN DEL jUEGO
+                    }
+                    userClicks.length = 0
+                    setTimeout(function() {
+                        generateComputerSequence()
+                        showComputerSequence()
+                    }, 1000)
+                }
+
+
+            }
+        })
+    })
+    return "<============== OK";
+
+    return '\'Allo \'Allo!';
 });
