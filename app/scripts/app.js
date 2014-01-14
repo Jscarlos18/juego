8cfbc1ae8af18506352c68dab4e3cd55ee87dc35
Jscarlos18 <jcquiancalle@hotmail.com>
2014-01-14 07:13:24 -0500

    reinicio

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,31 +1,32 @@
+/*global define */
 define([], function () {
     'use strict'
     
     var userPlaying = false
     var userClicks = new Array()
-    var buttonList
+    var botonList
     var computerSequence = new Array()
 
     var initialize = function() {
-        buttonList = jQuery.map( $(".boton"),
+        botonList = jQuery.map( $(".boton"),
                        function(element) {
                          return $(element).attr('id')
                        })
     }
 
     var generateComputerSequence = function() {
-        computerSequence.push( buttonList[
+        computerSequence.push( botonList[
                                  Math.floor(Math.random()
-                                   * buttonList.length)] )
+                                   * botonList.length)] )
         console.log(computerSequence)
     }
 
-    var highlight = function(button, color) {
-      var oldColor = button.css("background-color")
-      button.css("background-color", color).dequeue()
+    var highlight = function(boton, color) {
+      var oldColor = boton.css("background-color")
+      boton.css("background-color", color).dequeue()
             .delay(300)
             .queue( function() {
-                    button.css("background-color", oldColor).dequeue()
+                    boton.css("background-color", oldColor).dequeue()
                   })
     }
 
@@ -34,7 +35,7 @@ define([], function () {
         for(var id in seq) {
            (function(id){
              setTimeout( function() {
-               highlight($("#"+seq[id]), "#fff")//genera siguiente secuencia añadiendo uno mas a la secuencia anterior
+               highlight($("#"+seq[id]), "#fff")
              }, 600*id)
            })(id)
         }
@@ -44,49 +45,56 @@ define([], function () {
     }
 
     var compareSequences = function() {
-        // TODO
+        for(var i in userClicks) {
+            if(computerSequence[i] !== userClicks[i]) return false
+        }
         return true
     }
 
     var endGame = function() {
-       
+        var response = confirm("Error. Restart?")
+        if (response) {
+            userClicks.length = 0
+            computerSequence.length = 0
+            setTimeout( function() {
+                generateComputerSequence()
+                showComputerSequence()
+            }, 2000)
+        }
     }
     
     $(document).ready(function() {
         initialize()
 
-$('#start-button').click(function() {
-             $(this).css('color', '#f90').fadeOut()
-             setTimeout( function() {
-                 generateComputerSequence()
-                 showComputerSequence()
-             }, 500)
-         })
- 
+        $('#start-boton').click(function() {
+            $(this).css('color', '#fff').fadeOut()
+            setTimeout( function() {
+                generateComputerSequence()
+                showComputerSequence()
+            }, 500)
+        })
 
         $('.boton').click( function() {
             if(userPlaying) {
                 var thisId = $(this).attr('id')
-                highlight($(this), "#000")
+                highlight($(this), "#fff")
                 userClicks.push(thisId)
 
-                if (userClicks.length >= computerSequence.length) {//nº de clicks del jugador comparado con el nº de clicks de la computadora
-                    userPlaying = false 
-                    if (!compareSequences()){//si no son los correctos o se pasa de clicks se acaba el juego
-                        // FIN DEL jUEGO
-                    }
+                if (!compareSequences()) {
+                    userPlaying = false
+                    endGame()
+                }
+                if (userClicks.length >= computerSequence.length) {
+                    userPlaying = false
                     userClicks.length = 0
                     setTimeout(function() {
                         generateComputerSequence()
                         showComputerSequence()
                     }, 1000)
                 }
-
-
             }
         })
     })
+return computerSequence
     return "<============== OK";
-
-    return '\'Allo \'Allo!';
 });
