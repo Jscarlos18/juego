6f64616aa1226446d8c272666f5946159f1aee64
Jscarlos18 <jcquiancalle@hotmail.com>
2014-01-16 06:45:02 -0500

    fin del juego ya si que si

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,25 +8,25 @@ define([], function () {
     var computerSequence = new Array()
     var initialize = function() { 
         buttonList = jQuery.map( $(".boton"),
-                       function(element) {
-                         return $(element).attr('id')
-                       })
+        function(element) {
+        	return $(element).attr('id')
+        })
     }
     var generateComputerSequence = function() {
-        computerSequence.push( buttonList[
-                                 Math.floor(Math.random()
-                                   * buttonList.length)] )
+        computerSequence.push( buttonList[Math.floor(Math.random()* buttonList.length)] )
         console.log(computerSequence)
     }
+    
     //cambiar el color del css y volver al color original cuando finaliza
     var highlight = function(button, color) {
       var oldColor = button.css("background-color")
       button.css("background-color", color).dequeue()
-            .delay(300)
-            .queue( function() {
-                    button.css("background-color", oldColor).dequeue()
-                  })
+      	.delay(300)
+      	.queue( function() {
+      		button.css("background-color", oldColor).dequeue()
+         })
     }
+    
     // mostramos la secuecia generada aleatoria
     var showComputerSequence = function() {
         var seq = computerSequence
@@ -41,27 +41,27 @@ define([], function () {
             userPlaying = true
         }, 600*seq.length)
     }
+    
     //comparamos las secuencias de computer y user
     var compareSequences = function() {
-        // TODO
-        for(var i = 0; i < userClicks.length; ++i)
-                 {
-                         if(userClicks[i] != computerSequence[i])
-                         {
-                                 return false;
-                         }
-                 }
+        //tobias
+        for(var i = 0; i < userClicks.length; ++i){
+        	if(userClicks[i] != computerSequence[i]){
+        		return false;
+            }
+         }
+        
         //	 puntuacion
                  console.log("Correcto!");
                  $('#score').text("Puntuación "+(userClicks.length))
                  return true;
     }
+    
     //finalizacion del juego, e inicializamos los css originales
     var endGame = function() {
-        for(var i = 0; i < buttonList.length; i++)
-                 {
-                      $("#"+buttonList[i]).css("opacity", 0.3)
-                 }
+        for(var i = 0; i < buttonList.length; i++){
+        	$("#"+buttonList[i]).css("opacity", 0.3)
+        }
         $('#fail').html("Fin de la Partida")
         $('#fail').fadeIn(2000)
         $('#fail').fadeOut(2000)
@@ -69,18 +69,15 @@ define([], function () {
         userClicks.length=0;
         computerSequence.length=0;
         $('#start').css('background-color', '#000').fadeIn()
-
-
     }
     
     $(document).ready(function() {
         initialize()
 
         $('#start').click(function() {//quitamos la opacidad a los botones
-          for(var i = 0; i < buttonList.length; i++)
-                 {
-                      $("#"+buttonList[i]).css("opacity", 1)
-                 }
+          for(var i = 0; i < buttonList.length; i++){
+        	  $("#"+buttonList[i]).css("opacity", 1)
+          }
             $('#score').text("0");
             $(this).css('color', '#fff').fadeOut()
             setTimeout( function() {
